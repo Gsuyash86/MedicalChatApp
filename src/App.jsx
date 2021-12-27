72b85fa660944651f6bc692c12999b7a39db4048
Suyash gupta <89170907+Gsuyash86@users.noreply.github.com>
2021-12-27 10:32:42 +0530

    BackendBuilt

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { ChannelList, Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import {
+  ChannelContainer,
+  ChannelListContainer,
+  Auth,
+} from "./components/index";
+
+import "./App.css";
+
+const apiKey = process.env.REACT_APP_STREAM_API_KEY;
+
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
+
+console.log(client);
 
 const App = () => {
+  if (!authToken) return <Auth />;
+
   return (
-    <div>
-      <h1>My App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
